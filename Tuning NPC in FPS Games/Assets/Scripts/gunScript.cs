21c0c422b9a3b7d359a3000deb6fe8808752b84f
HanzalaZahid <hanzlamirza@live.com>
2020-02-23 18:52:23 +0500

    Guns and character scripts

---
+++ Tuning NPC in FPS Games/Assets/Scripts/gunScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+        
+    }
+
+    void shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
